d4c631a8b0c69fb2da266ffbc4e3fce4ddc54fa1
Tomaz Lovrec <tomaz.lovrec@form3.tech>
2020-10-02 10:19:41 +0200

    add comments to exported members

--- config.go
+++ config.go
@@ -5,10 +5,17 @@ import (
 	"time"
 )
 
+// Config struct for the HTTP server.
 type Config struct {
+	// SockFile defines the location of the socket file for the listener.
 	SockFile string
-	Addr     string
-	Timeout  time.Duration
+
+	// Addr of the listener.
+	Addr string
+
+	// Timeout controls the shutdown timeout. If the server takes longer to shut
+	// down, all open connections will be dropped.
+	Timeout time.Duration
 
 	ln net.Listener
 }
